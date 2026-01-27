5fe94a1a7084902192c2ea8c06d4dacdb6b94f6f
niendzvdtg01 <duynien34@gmail.com>
2026-01-27 11:20:06 +0700

    fix cors

--- src/main/java/com/gallery/backend/Controllers/AthController.java
+++ src/main/java/com/gallery/backend/Controllers/AthController.java
@@ -13,8 +13,6 @@ import com.gallery.backend.entity.Users;
 import com.gallery.backend.security.JwtsUtil;
 import com.gallery.backend.services.AuthService;
 
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +24,14 @@ public class AthController {
     private AuthService authService;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         Users user = authService.athencate(request.getUsername(), request.getUserpassword());
 
         String token = JwtsUtil.generateToken(user);
         // Tao response cookie
+        if (user == null) {
+            return ResponseEntity.status(401).body("Login failed");
+        }
         ResponseCookie cookie = ResponseCookie.from("access_cookie", token)
                 .httpOnly(true)
                 .secure(true)
@@ -39,7 +40,6 @@ public class AthController {
                 .maxAge(Duration.ofMinutes(60))
                 .build();
 
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-        return ResponseEntity.ok("Login successfully!!");
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body("Login succesfully!");
     }
 }
